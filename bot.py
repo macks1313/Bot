e818cda66180a58d1d63f11efdfd18efd6969f9d
macks1313 <maximefeger93@gmail.com>
2025-01-30 02:18:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_SECRET")  # Modifié pour correspondre à ta configuration
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Authentification Twitter
@@ -79,4 +79,3 @@ def main():
 # Lancer le script
 if __name__ == "__main__":
     main()
-
